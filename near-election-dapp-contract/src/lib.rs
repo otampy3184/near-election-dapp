26c2d3d1cb301cba01bba6e2706b7eb7352b569a
django_freeman <o.tampodo.t.18@gmail.com>
2022-12-11 23:40:18 +0900

    利用ライブラリ更新

--- near-election-dapp-contract/src/lib.rs
+++ near-election-dapp-contract/src/lib.rs
@@ -1,7 +1,19 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::collections::{LazyOption, LookupMap, UnorderedMap, UnorderedSet};
+use near_sdk::json_types::U128;
+use near_sdk::serde::{Deserialize, Serialize};
+use near_sdk::{env, near_bindgen, AccountId, Balance, CryptoHash, PanicOnDefault, Promise};
+
+mod vote;
+mod enumeration;
+mod internal;
+mod metadata;
+mod mint;
+mod nft_core;
+
+pub use crate::enumeration::*;
+use crate::internal::*;
+pub use crate::metadata::*;
+pub use crate::mint::*;
+pub use crate::nft_core::*;
+pub use vote::*;
